b2c9aa1304de7645f3bf783ca5d296b033a17909
ihihiabde <81963275+ihihiabde@users.noreply.github.com>
2022-07-20 19:27:24 +0100

    Update settings.py

--- pfe/settings.py
+++ pfe/settings.py
@@ -123,21 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-#STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
-
+STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+SETTINGS_DIR = os.path.dirname(__file__)
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 
 django_heroku.settings(locals())
