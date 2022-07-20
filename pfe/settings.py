0aa3b287c1cce9c8912d92d2c11b016bcf156937
ihihiabde <81963275+ihihiabde@users.noreply.github.com>
2022-07-20 19:50:22 +0100

    Update settings.py

--- pfe/settings.py
+++ pfe/settings.py
@@ -128,12 +128,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_HOST = os.environ.get("eatfitness.herokuapp.com", "")
-STATIC_URL = STATIC_HOST + "/static/"
+
 
 
 
