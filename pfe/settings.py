c86f9d518f084aa7302ce4f2cfeeedc35bf7494d
ihihiabde <81963275+ihihiabde@users.noreply.github.com>
2022-07-20 19:54:02 +0100

    Update settings.py

--- pfe/settings.py
+++ pfe/settings.py
@@ -119,6 +119,7 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
